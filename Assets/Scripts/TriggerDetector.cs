f67b893fb24b51cac2876f3ab2d86e82d667aeb6
steve rector <steve.rector@datadoghq.com>
2022-10-31 12:52:35 -0700

    Adds working raycast that detects ghosts
    cast comes out of camera

---
+++ Assets/Scripts/TriggerDetector.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDetector : MonoBehaviour
+{
+    [SerializeField] Collider playerCollider;
+    [SerializeField] LayerMask ghostLayerMask;
+    [SerializeField] int rayCastDistance = 100;
+
+    private GameObject lastHitGameObject;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        CheckForGhosts();
+    }
+
+    private void CheckForGhosts()
+    {
+        Vector3 viewport = new Vector3(0.5f, 0.5f, 0f);
+        Ray ray = cam.ViewportPointToRay(viewport);
+        RaycastHit hit;
+
+        ray.origin = cam.transform.position;
+
+        if (Physics.Raycast(ray, out hit, rayCastDistance, ghostLayerMask))
+        {
+            lastHitGameObject = hit.transform.gameObject;
+            Debug.Log(lastHitGameObject);
+
+            //Debug.DrawLine(cam.transform.position, hit.transform.position, Color.red);
+        }
+    }
+}
+
